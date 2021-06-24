315e26fa60a758d75bd019f9e6d29bc1db6eabe0
laptevaarina <arina.lapteva2016@yandex.ru>
2021-06-25 01:05:18 +0300

    new commit

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,3 +1,5 @@
+from hashlib import md5
+from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -11,6 +13,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     password_hash = db.Column(db.String(128))
+    events = db.relationship('Events', backref = 'sport', lazy = 'dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default = datetime.utcnow())
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -21,3 +26,16 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+class Events(db.Model):
+    id =  db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Event {}>'.format(self.body)
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,39 +1,51 @@
-from flask import render_template, flash, redirect, request
+from datetime import datetime
+from flask import render_template, flash, redirect, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from werkzeug.urls import url_parse
-
-
 
 @app.route('/')
 @app.route('/general')
 def general():
-    return render_template('general.html', title = 'Sport web-site')
+    events = [
+        {
+            'sport' : 'football',
+            'body' : '17:40'
+        },
+        {
+            'sport': 'run',
+            'body': '12:00'
+        }
+    ]
+    if current_user.is_authenticated:
+        user = User.query.filter_by(username=current_user.username).first_or_404()
+        return render_template('general.html', title='Sport web-site', user=user, events = events)
+    return render_template('general.html', title = 'Sport web-site', events = events)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect('/profile')
+        return redirect(url_for('general'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username = form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect('/login')
+            return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        #next_page = request.args.get('next')
-        #if not next_page or url_parse(next_page).netloc != '':
-        #    next_page = 'general'
-        #return redirect(next_page)
-        return redirect('/profile') #переход в профиль?
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = 'general'
+        return redirect(url_for(next_page))
+        #return redirect('/general') #переход в профиль?
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect('general')
+        return redirect(url_for('general'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -41,15 +53,40 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect('/login')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/logout') #на странице профиля (html добавить)?
 def logout():
     logout_user()
-    return redirect('/general')
+    return redirect(url_for('general'))
 
-@app.route('/profile') #profile ещё нет
+@app.route('/user/<username>') #profile есть
 @login_required
-def profile():
-    pass
+def profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    events = [ {'chel' : user, 'exercise' : 'анжумания'},
+                 {'chel' : user, 'exercise' : 'прес качат'} ]
+    return render_template('profile.html', user = user, events = events)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/edit_profile', methods = ['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
