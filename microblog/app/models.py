f68d889725bf484421915a8139ef0678bdb81f26
laptevaarina <arina.lapteva2016@yandex.ru>
2021-06-25 18:10:26 +0300

    new commit

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -4,18 +4,31 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 @login.user_loader
 def load_user(id):
-    return User.query.get(int(id)) #????
+    return User.query.get(int(id))
+
+
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
+
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), index = True, unique = True)
-    email = db.Column(db.String(120), index = True, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    events = db.relationship('Events', backref = 'sport', lazy = 'dynamic')
+    events = db.relationship('Events', backref='sport', lazy='dynamic')
     about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default = datetime.utcnow())
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow())
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -31,8 +44,27 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Events.query.join(
+            followers, (followers.c.followed_id == Events.user_id)).filter(
+            followers.c.follower_id == self.id).order_by(
+            Events.timestamp.desc())
+
+
 class Events(db.Model):
-    id =  db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import render_template, flash, redirect, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from app.models import User
 from werkzeug.urls import url_parse
 
@@ -67,7 +67,8 @@ def profile(username):
     user = User.query.filter_by(username=username).first_or_404()
     events = [ {'chel' : user, 'exercise' : 'анжумания'},
                  {'chel' : user, 'exercise' : 'прес качат'} ]
-    return render_template('profile.html', user = user, events = events)
+    form = EmptyForm()
+    return render_template('profile.html', user = user, events = events, form = form)
 
 @app.before_request
 def before_request():
@@ -78,7 +79,7 @@ def before_request():
 @app.route('/edit_profile', methods = ['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
@@ -90,3 +91,41 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = username).first()
+        if user is None:
+            flash('User {} not found'.format(username))
+            return redirect(url_for('general'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('profile', username = username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {}!'.format(username))
+        return redirect(url_for('profile', username = username))
+    else:
+        return redirect(url_for('general'))
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found'.format(username))
+            return redirect(url_for('general'))
+        if user == current_user:
+            flash('You cannot unfollow yourself')
+            return redirect(url_for('profile', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('You are not following {}'.format(username))
+        return redirect(url_for('profile', username=username))
+    else:
+        return redirect(url_for('general'))
